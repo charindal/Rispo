9f42fe9b83393c0069922742ae6bea385d516776
charindal <charindal@gmail.com>
2026-01-12 14:05:52 +0200

    fix cors and redeploy

--- src/main/java/za/co/infratech/rispo/config/SecurityConfig.java
+++ src/main/java/za/co/infratech/rispo/config/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "http://localhost:*"));
+        // Allow all origins (Traefik handles the routing)
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
